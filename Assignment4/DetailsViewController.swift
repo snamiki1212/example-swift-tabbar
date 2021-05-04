4c217a4bac39f7e3dd06f8056e6113acc93d26d3
snamiki1212 <snamiki1212@gmail.com>
2021-05-03 21:30:08 -0700

    Refactor logic

--- Assignment4/DetailsViewController.swift
+++ Assignment4/DetailsViewController.swift
@@ -13,13 +13,11 @@ class DetailsViewController: UIViewController {
     func pushLabel(text: String) -> UIView {
         let view = UIView()
         let label = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 20))
-        
         label.text = text
         label.textAlignment = .center
         view.addSubview(label)
         label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        
         return view
     }
 
@@ -27,35 +25,31 @@ class DetailsViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
 
-        let list: [UIView] = {
-            // TODO: refactor
-            if let previousVC = navigationController?.viewControllers[0] {
-                if let cityVC = previousVC as? CityViewController {
-                    if let city = cityVC.city {
-                        var list = [UIView]()
-                        
-                        list.append(pushLabel(text: "Country"))
-                        list.append(pushLabel(text: city.icon))
-                        
-                        list.append(pushLabel(text: "City"))
-                        list.append(pushLabel(text: city.name))
-                        
-                        list.append(pushLabel(text: "Tempature"))
-                        list.append(pushLabel(text: String(city.temp)))
-                        
-                        list.append(pushLabel(text: "Summary"))
-                        list.append(pushLabel(text: city.summary))
+        let subviews: [UIView] = {
+            if let previousVC = navigationController?.viewControllers[0],
+               let cityVC = previousVC as? CityViewController,
+               let city = cityVC.city
+            {
+                var list = [UIView]()
+                
+                list.append(pushLabel(text: "Country"))
+                list.append(pushLabel(text: city.icon))
+                
+                list.append(pushLabel(text: "City"))
+                list.append(pushLabel(text: city.name))
+                
+                list.append(pushLabel(text: "Tempature"))
+                list.append(pushLabel(text: String(city.temp)))
+                
+                list.append(pushLabel(text: "Summary"))
+                list.append(pushLabel(text: city.summary))
 
-                        return list
-                    }
-                }
+                return list
             }
             return []
         }()
         
-        print(list)
-        
-        let stack = UIStackView(arrangedSubviews: list)
+        let stack = UIStackView(arrangedSubviews: subviews)
         stack.translatesAutoresizingMaskIntoConstraints = false
         stack.axis = .vertical
         stack.spacing = 10
